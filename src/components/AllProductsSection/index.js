3c0fd74ed9406d15b6cda329e9cb5ebb496df62e
Madhu <mulaglam@gmail.com>
2022-05-13 05:20:24 +0000

    displaying all products using api

---
+++ src/components/AllProductsSection/index.js
@@ -0,0 +1,56 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import ProductCard from '../ProductCard'
+import './index.css'
+
+class AllProductsSection extends Component {
+  state = {
+    productsList: [],
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+
+  getProducts = async () => {
+    const accessToken = Cookies.get('jwt_token')
+    const apiUrl = 'https://apis.ccbp.in/products'
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+    const response = await fetch(apiUrl, options)
+    const fetchedData = await response.json()
+    const updatedData = fetchedData.products.map(product => ({
+      id: product.id,
+      title: product.title,
+      brand: product.brand,
+      price: product.price,
+      imageUrl: product.image_url,
+      rating: product.rating,
+    }))
+    this.setState({productsList: updatedData})
+  }
+
+  renderProductsList = () => {
+    const {productsList} = this.state
+    return (
+      <div>
+        <h1 className="products-list-heading">All Products</h1>
+        <ul className="products-list">
+          {productsList.map(product => (
+            <ProductCard productData={product} key={product.id} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  render() {
+    return <>{this.renderProductsList()}</>
+  }
+}
+
+export default AllProductsSection
